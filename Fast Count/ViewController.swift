b695e764ca28aac8312c3f288cc4bf7b77cc1e52
Royer Ramirez Ruiz <RoyerRamirezRuiz@royers-macbook-pro.local>
2017-06-26 22:36:37 -0500

    Initial Commit

---
+++ Fast Count/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fast Count
+//
+//  Created by Royer Ramirez Ruiz on 6/26/17.
+//  Copyright © 2017 EntegrityEnergyPartners. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
